9a0e0cdac4200706410a55da63229e9c52396682
shivangisoni98 <51941309+shivangisoni98@users.noreply.github.com>
2019-06-18 10:17:40 +0530

    Add files via upload

---
+++ TestValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using mvctest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvctest.Validator
+{
+    public class TestValidator : AbstractValidator<Test>
+    {
+        public TestValidator()
+
+        {
+
+            //RuleFor(x => x.CouponCode).NotEmpty().WithMessage("Coupon Code is required");
+            RuleFor(x => x.CouponCode).Equal("ABCD");
+        }
+    }
+}
