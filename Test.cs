22c71202cbba8f4331149d3c5cd4a2ec495e596e
shivangisoni98 <51941309+shivangisoni98@users.noreply.github.com>
2019-06-18 10:17:20 +0530

    Add files via upload

---
+++ Test.cs
@@ -0,0 +1,19 @@
+﻿using SuperCaptcha.Mvc.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace mvctest.Models
+{
+    public class Test
+    {
+        public string CouponCode { get; set; }
+        public bool b { get; internal set; }
+
+        [Required(ErrorMessage = "Captcha is required")]
+        [VerifyCaptcha]
+        public string CaptchaText { get; set; }
+    }
+}
